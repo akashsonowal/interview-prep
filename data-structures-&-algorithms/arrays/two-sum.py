61e39b84eb877fc7ef0ad15f043978683e146352
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2022-12-29 11:09:11 +0530

    Update two-sum.py

--- data-structures-&-algorithms/arrays/two-sum.py
+++ data-structures-&-algorithms/arrays/two-sum.py
@@ -1,1 +1,11 @@
+# O(n) time | O(n) space
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[num] = i
+        return
