934419328860f7e96c5e46ddeac45c1cbfcb6f34
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-01-09 08:35:11 +0530

    Create unique-paths.py

---
+++ data-structures-&-algorithms/dynamic-programming/unique-paths.py
@@ -0,0 +1,13 @@
+#O(m*n) time | O(n) space
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for i in range(m - 2, -1, -1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+
+        return row[0]
