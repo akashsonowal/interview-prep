7d27a62225c36dd2f547cc662924cf524958a23c
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-01-17 08:40:05 +0530

    Create edit_distance.py

---
+++ data-structures-&-algorithms/dynamic-programming/edit_distance.py
@@ -0,0 +1,23 @@
+# O(m * n) time | O(m * n) space
+
+class Solution:
+    @staticmethod
+    def minDistance(word1: str, word2: str) -> int:
+        cache = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
+
+        for j in range(len(word2) + 1):
+            cache[len(word1)][j] = len(word2) - j
+        for i in range(len(word1) + 1):
+            cache[i][len(word2)] = len(word1) - i
+        
+        for i in range(len(word1) - 1, -1, -1):
+            for j in range(len(word2) - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    cache[i][j] = cache[i + 1][j + 1]
+                else:
+                    cache[i][j] = 1 + min(cache[i + 1][j], cache[i][j + 1], cache[i + 1][j + 1])
+        return cache[0][0]
+
+word1 = "intention",
+word2 = "execution"
+assert Solution.minDistance(word1, word2) == 5
