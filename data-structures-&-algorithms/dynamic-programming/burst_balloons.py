276a8067901bd663ffc39f6ff51344df721f9dca
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-01-19 08:33:24 +0530

    Update burst_balloons.py

--- data-structures-&-algorithms/dynamic-programming/burst_balloons.py
+++ data-structures-&-algorithms/dynamic-programming/burst_balloons.py
@@ -1,6 +1,7 @@
 # O(n3) time | O(n2) space
 
 class Solution:
+    
     @staticmethod
     def maxCoins_recursive(nums: List[int]) -> int:
         cache = {}
@@ -20,3 +21,17 @@ class Solution:
             return cache[(l, r)]
 
         return dfs(1, len(nums) - 2)
+    
+    @staticmethod
+    def maxCoins_iterative(self, nums: List[int]) -> int:
+        cache = {}
+        nums = [1] + nums + [1]
+
+        for offset in range(2, len(nums)):
+            for left in range(len(nums) - offset):
+                right = left + offset
+                for pivot in range(left + 1, right):
+                    coins = nums[left] * nums[pivot] * nums[right]
+                    coins += cache.get((left, pivot), 0) + cache.get((pivot, right), 0)
+                    cache[(left, right)] = max(coins, cache.get((left, right), 0))
+        return cache.get((0, len(nums) - 1), 0)
