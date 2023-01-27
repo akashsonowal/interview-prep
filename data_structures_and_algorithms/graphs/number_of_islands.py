df15842855f39a1497bc341c28e15a01c3f8011d
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-01-27 08:42:00 +0530

    Update number_of_islands.py

--- data_structures_and_algorithms/graphs/number_of_islands.py
+++ data_structures_and_algorithms/graphs/number_of_islands.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution: #bfs is iteratove
     @staticmethod
     def numIslands_bfs(grid: List[List[str]]) -> int:
         if not grid:
@@ -33,6 +33,37 @@ class Solution:
                     bfs(r, c)
                     islands += 1
 
+        return islands
+ 
+class Solution: #dfs is recursive
+    @staticmethod
+    def numIslands_dfs(grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        islands = 0
+        visited = set()
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if (
+                r not in range(rows)
+                or c not in range(cols)
+                or grid[r][c] == "0"
+                or (r, c) in visited
+            ):
+                return
+
+            visited.add((r, c))
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    islands += 1
+                    dfs(r, c)
         return islands
       
       
