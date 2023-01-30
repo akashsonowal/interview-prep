a08b715276f01000e7c5012fcabc9b42142f6b77
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-01-30 09:32:10 +0530

    Create course_schedule.py

---
+++ data_structures_and_algorithms/graphs/course_schedule.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i: [] for i in range(numCourses)}
+
+        for course, prerequisite in prerequisites:
+            preMap[course].append(prerequisite)
+
+        visited = set()
+        def dfs(course):
+            if course in visited:
+                return False
+            if preMap[course] == []:
+                return True
+            visited.add(course)
+            for pre in preMap[course]:
+                if not dfs(pre):
+                    return False
+            visited.remove(course)
+            preMap[course] = []
+            return True  
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True  
+                
+
