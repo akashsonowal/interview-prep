792f1198c64d9d1912464da11fd880da0ea51323
Akash Sonowal <work.akashsonowal@gmail.com>
2023-04-27 17:48:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,18 @@ import re
 from typing import List, Tuple
 
 from setuptools import setup, find_packages
+
+
+NAME = "ml-engineering-foundations"
+DESCRIPTION = "One-stop resource for ML engineering"
+
+URL = "https://github.com/akashsonowal/ml-engineering-foundations"
+AUTHOR =  "Akash Sonowal"
+REQUIRES_PYTHON = ">=3.9.0"
+HERE = PATH(__file__).parent
+
+try:
+  with open(HERE / "readme.md", encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+except FileNotFoundError:
+  long_description = DECRIPTION
