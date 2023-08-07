a5b237a38b4ef26c8627f4a4ebd262990527966b
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-07 15:33:12 +0530

    update

--- interview_prep/coding/get_permutations.py
+++ interview_prep/coding/get_permutations.py
@@ -5,16 +5,21 @@ def get_permutations(nums):
     if len(nums) <= 1:
         return [nums] 
     else:
+        for i in nums:
+            prev = nums[:i]
+            next_ = nums[i + 1:]
+            permutations.append(get_permutations(prev) + i + get_permutations(next_))
         return permutationss
 
 if __name__ == "__main__":
-    a = [2, 3, 4]
+    pass
+#     a = [2, 3, 4]
 
 
-(1, 2, 3, 4)
+# (1, 2, 3, 4)
 
-2 -> (2, 3, 4) -> (2, 4, 3)
-3 -> (4, 3, 2)
-4 -> (3, 2, 4)
-(4, 2, 3)
-(3, 4, 2)
+# 2 -> (2, 3, 4) -> (2, 4, 3)
+# 3 -> (4, 3, 2)
+# 4 -> (3, 2, 4)
+# (4, 2, 3)
+# (3, 4, 2)
