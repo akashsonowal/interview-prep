c562b3fbefe029b7a25453a2e4d1e3dad67452ca
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-12 02:32:12 -0700

    update

---
+++ interview_prep/coding/ml_applications/tldr.py
@@ -0,0 +1,7 @@
+# extractive summarization
+# evaluate using ROUGE (Recall Oriented Understudy for Gisting Evaluation)
+
+# ROUGE-L f-score is used for comparing ground truth summary to model summary
+# ROUGE-L computes the longest common subsequence b/w two texts. The longer the common subsequence the better it is
+# the precision and recall of ROUGE-L will be used.
+
