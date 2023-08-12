91615a5074459b7f7122829f916a9dd9921b8825
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-12 04:04:43 -0700

    update

--- interview_prep/coding/ml_applications/tldr.py
+++ interview_prep/coding/ml_applications/tldr.py
@@ -5,3 +5,18 @@
 # ROUGE-L computes the longest common subsequence b/w two texts. The longer the common subsequence the better it is
 # the precision and recall of ROUGE-L will be used.
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np 
+from nltk import sent_tokenize
+from nltk.corpus import stopwords 
+
+def tldr(text_to_summarize):
+    sentence_tokens = np.array(sent_tokenize(text_to_summarize))
+    stop_word_set = set(stopwords.words("english"))
+
+    tf_idf_vectorizer = TfidfVectorizer(stop_words=stop_word_set)
+    
+    pass 
+
+if __name__ == "__main__":
+    text_to_summarize = "Hi I am Akash and I am an aspiring Research Engineer. I love to solve complex problems at scale using AI"
