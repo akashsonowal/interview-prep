719da942e178016df1ff2d77ff4d154e7605678d
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-29 09:22:00 +0530

    update

--- interview_prep/coding/company_questions/harman.py
+++ interview_prep/coding/company_questions/harman.py
@@ -1,3 +1,5 @@
+# Data Scientist (NLP) 
+
 # swap two numbers without using a third variable
 def swap(a, b):
     a = a + b 
@@ -5,5 +7,10 @@ def swap(a, b):
     a = a - b 
     return a, b
 
+# sort using lambda
+def sorting_with_lambda(x):
+    return sorted(x, key=lambda x: x["model"], reverse=True)
+
 if __name__ == "__main__":
     assert 2, 3 == swap(3, 2)
+    assert [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}, {'make': 'Mi Max', 'model': 2, 'color': 'Gold'}] == sorting_with_lambda([{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}])
