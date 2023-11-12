4e287c3f6d2d6b689a61429debb03ffbf99da6c8
Akash Sonowal <work.akashsonowal@gmail.com>
2023-11-12 15:55:25 +0530

    update

---
+++ interview_prep/coding/dsa/insertion_sort.py
@@ -0,0 +1,23 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    # Implementation of Insertion Sort
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        n = len(pairs)
+        res = []  # To store the intermediate states of the array
+        
+        for i in range(n):
+            j = i - 1
+
+            # Move elements that are greater than key one position ahead
+            while j >= 0 and pairs[j].key > pairs[j + 1].key:
+                pairs[j], pairs[j + 1] = pairs[j + 1], pairs[j]
+                j -= 1
+            
+            # Clone and save the entire state of the array at this point
+            res.append(pairs[:])
+
+        return res
