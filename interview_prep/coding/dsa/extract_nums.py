6c5df906b35ebba2a3144050cc289581bdb7e60a
Akash Sonowal <work.akashsonowal@gmail.com>
2023-12-06 00:04:21 +0530

    Update extract_nums.py

--- interview_prep/coding/dsa/extract_nums.py
+++ interview_prep/coding/dsa/extract_nums.py
@@ -8,16 +8,16 @@ class Solution:
         res = []
 
         while i < len(ip):
-        if ip[i].isdigit():
-            l = r = i
-            while l >= 0 and ip[l].isdigit():
-            l -= 1
-            while r < len(ip) and ip[r].isdigit():
-            r += 1
-            res.append(ip[l + 1: r])
-            i = r
-        else:
-            i += 1 
+            if ip[i].isdigit():
+                l = r = i
+                while l >= 0 and ip[l].isdigit():
+                    l -= 1
+                while r < len(ip) and ip[r].isdigit():
+                    r += 1
+                res.append(ip[l + 1: r])
+                i = r
+            else:
+                i += 1 
             
         return res
 
