9505cc99a8166b638c486769d0f2d203baa52d00
Akash Sonowal <work.akashsonowal@gmail.com>
2023-12-05 23:59:53 +0530

    update

--- interview_prep/coding/dsa/extract_nums.py
+++ interview_prep/coding/dsa/extract_nums.py
@@ -1,23 +1,29 @@
 """
 Techolution (AI Engineer)
 """
-ip = "avdcfsd3212ad123ad"
+class Solution:
+    @staticmethod
+    def extract_nums(ip):
+        i = 0
+        res = []
 
-op = ["3212", "123"]
+        while i < len(ip):
+        if ip[i].isdigit():
+            l = r = i
+            while l >= 0 and ip[l].isdigit():
+            l -= 1
+            while r < len(ip) and ip[r].isdigit():
+            r += 1
+            res.append(ip[l + 1: r])
+            i = r
+        else:
+            i += 1 
+            
+        return res
 
-i = 0
-res = []
+if __name__ == '__main__': 
 
-while i < len(ip):
-  if ip[i].isdigit():
-    l = r = i
-    while l >= 0 and ip[l].isdigit():
-      l -= 1
-    while r < len(ip) and ip[r].isdigit():
-      r += 1
-    res.append(ip[l + 1: r])
-    i = r
-  else:
-    i += 1 
+    ip = "avdcfsd3212ad123ad"
+    op = ["3212", "123"]
 
-assert res == op
+    assert Solution.extract_nums(ip) == op
